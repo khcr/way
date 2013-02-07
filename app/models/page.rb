08e8c344857615962914319738e6910e144e059f
khcr <kocher.ke@gmail.com>
2013-02-07 21:23:25 +0100

    final

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,6 +2,6 @@ class Page < ActiveRecord::Base
   attr_accessible :content, :title, :name
 
   validates :content, presence: true
-  validates :title, presence: true, length: { maximum: 10 }
-  validates :name, presence: true, length: { maximum: 10 }, uniqueness: true
+  validates :title, presence: true, length: { maximum: 15 }
+  validates :name, presence: true, length: { maximum: 15 }
 end
