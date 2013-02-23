d592ffc3437d0ec5a6fb98a1d5100319fdad3bc2
Noémien <nkcr.je@gmail.com>
2013-02-23 15:51:36 +0100

    cleaning

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
+<<<<<<< HEAD
 //= require jquery-fileupload/basic
 //= require jquery-fileupload/vendor/tmpl
 //= require_tree .
+=======
+//= require_tree .
+>>>>>>> cleaning
