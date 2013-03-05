5d2cdb0e1989c83c3bd6cb49e5fec8b28fc11c2b
khcr <kocher.ke@gmail.com>
2013-03-05 18:31:32 +0100

    mercury editor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Way::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( fr.locale.js.coffee )
+  config.assets.precompile += %w( fr.locale.js )
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
