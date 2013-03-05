ca637a2cba424584454baad430923ca806e8897f
khcr <kocher.ke@gmail.com>
2013-03-05 16:23:20 +0100

    mercury editor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,11 +23,11 @@ Way::Application.configure do
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
-  config.assets.precompile += %w( mercury.js mercury.css )
+  config.assets.precompile += %w( mercury.js mercury.css fr.locale.js.coffee jquery-1.7.js )
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
