c758a222f3a710bd6dad06d16db21931c67954aa
khcr <kocher.ke@gmail.com>
2013-03-05 18:34:44 +0100

    mercury editor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Way::Application.configure do
   config.assets.compile = true
 
   # Generate digests for assets URLs
-  config.assets.digest = false
+  config.assets.digest = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
