4e72c95a93c577ba42a510df82a593abcb2351e0
khcr <kocher.ke@gmail.com>
2013-03-06 22:33:45 +0100

    db

--- db/migrate/20130306173535_change_level_from_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeLevelFromUsers < ActiveRecord::Migration
-  def change
-  	change_column :users, :level, :string
-  end
-end
--- db/migrate/20130306174657_change_from_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeFromUsers < ActiveRecord::Migration
-  def change
-  	change_column :users, :level, :integer
-  end
-end
