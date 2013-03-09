fff035506add4e7019e59a28d96b4981d4a8d5c9
Noémien <nkcr.je@gmail.com>
2013-03-09 12:08:25 +0100

    Design clean, gif upload

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -6,7 +6,7 @@ class Admin::EventsController < ApplicationController
 	layout 'admin'
 
 	def index
-		@events = Event.page(params[:page]).per_page(10)
+		@events = Event.page(params[:page]).order('date ASC').per_page(10)
 	end
 
 	def new 
