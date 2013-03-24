98a409181ad0d84be10f0b6effecc0ccf5e08b82
khcr <kocher.ke@gmail.com>
2013-03-24 21:19:48 +0100

    fix events routes

--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,7 @@ Way::Application.routes.draw do
     resources :galleries, only: [:index, :show], path: '/medias'
     resources :sessions, only: [:create, :destroy]
     resources :projects, only: [:show, :index], path: '/activites'
-    resources :events, only: [:show, :index], :constraints => {:id => /[a-z]+/}
+    resources :events, only: [:show, :index], :constraints => {:id => /(?:[a-z]|-)*/}
 
   end
 
