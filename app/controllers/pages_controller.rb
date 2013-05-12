5f7b0d0ef7805dac021ec10ab64d9b04ae5a9183
Noémien <nkcr.je@gmail.com>
2013-05-12 19:19:09 +0200

    atom feed order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
 		@presentation = Page.find_by_name('presentation')
 		@news = New.where('date_exp >=?', [Time.zone.now]).order('date_exp ASC')
 		@galleries = Gallery.order('date DESC').limit(2)
-		@next_event = Event.where('date >=?', [Time.zone.now]).order('date ASC').first
+		@next_event = Event.where('date >=?', [Time.zone.now]).order('date DESC').first
 		@slideshows = Slideshow.where('date_exp>=?', [Time.zone.now]).order('date_exp ASC')
 	end
 
