68d36c22d0bce09781ff860d8b7fa0601fed813c
Noémien <nkcr.je@gmail.com>
2014-01-07 20:11:53 +0100

    Private gallery feature

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -5,15 +5,32 @@ class GalleriesController < ApplicationController
 	before_filter :find_page, only: [:show]
 
 	def index
-		@galleries = Gallery.order('date DESC').page(params[:page]).per_page(8)
+		@galleries = Gallery.order('date DESC').where('isPrivate = ?', false).page(params[:page]).per_page(8)
 	end
 
 	def show
+	end
+
+	# log with key
+	def create
+		@gallery = Gallery.find(params[:gallery][:id])
+		if @gallery.key == params[:gallery][:key]
+			render 'show'
+		else
+			flash.now[:error] = "Clef incorrect."
+			render 'auth'
+		end
+	end
+
+	def auth
+		@gallery = Gallery.find(params[:id])
 	end
 
 	private 
 	
 	def find_page
   	@gallery = Gallery.find_by_slug!(params[:id])
+  	signed_in_gallery(@gallery) if @gallery.isPrivate
 	end
+
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,5 +1,5 @@
 class Gallery < ActiveRecord::Base
-  attr_accessible :name, :description, :date
+  attr_accessible :name, :description, :date, :isPrivate, :key
   has_many :paintings, :dependent => :destroy 
 
   validates :name, presence: true, length: { maximum: 55 }
